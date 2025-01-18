82d5186a078e9f0b0897770aea6a46736f4b35d5
Ryan C. Durham <ryan@stagerightlabs.com>
2025-01-18 08:45:41 -0800

    Add Website model with sushi

---
+++ src/Website.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Relay;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property string $name
+ * @property string $link
+ * @property string $category
+ * @property ?string $league
+ * @property ?Feed $feed
+ * @property ?string $rss
+ * @property ?string $relay
+ */
+final class Website extends Model
+{
+    use \Sushi\Sushi;
+
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'feed' => Feed::class,
+        ];
+    }
+
+    /**
+     * The relay RSS url.
+     *
+     * @return Attribute
+     */
+    protected function relay(): Attribute
+    {
+        return Attribute::make(
+            get: fn() => $this->feed ? route('feed', $this->feed) : null
+        );
+    }
+
+    /**
+     * The website data.
+     */
+    protected array $rows = [
+        [
+            'name' => 'NWSL Soccer',
+            'link' => 'https://www.nwslsoccer.com/',
+            'category' => 'news',
+            'league' => 'nwsl',
+            'feed' => Feed::NWSL
+        ],
+    ];
+}
