383d0c6a05864787378093d65a816f9cfd0ecc92
Ryan C. Durham <ryan@stagerightlabs.com>
2025-01-19 13:48:29 -0800

    Add umami tracking code

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,6 +5,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>WoSo RSS Relay</title>
         @vite("resources/css/app.css")
+        <script
+            defer
+            src="https://umami.stagerightlabs.com/script.js"
+            data-website-id="e932c63c-76d0-4565-910c-5df71f6b327f"
+        ></script>
     </head>
     <body
         class="flex h-screen items-center justify-center bg-slate-100 align-middle dark:bg-slate-800"
